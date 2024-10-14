f63efa051360505a0a20eb3fa73d3c4d5af729bc
PernacurtaXD <168147492+PernacurtaXD@users.noreply.github.com>
2024-10-14 19:08:47 +0000

    banco de dados

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Python_BD
+#  Aula de CRUD: BANCO DE DADOS 
+
+
+## Objetivo:
+Criar conexão com banco de dados e realizar operações básicas (CRUD)
+
+## Tecnologias:
+ORM: SQLAlchemy
+Banco de dados: SQLite
+
+## Detalhes:
+Criando conexão com banco de dados SQLite, usando o SQLAlchemy como ferramenta ORM para interação com banco de dados 
+
---
+++ principal.py
@@ -0,0 +1,47 @@
+import os 
+from sqlalchemy import create_engine, Column, String, Integer 
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+# Criando Banco de dados 
+MEU_BANCO = create_engine("sqlite:///meubancozinho.db")
+
+# Criando conexão com banco de dados 
+Session = sessionmaker(bind=MEU_BANCO)
+session = Session()
+
+# Criando tabela 
+Base = declarative_base() #Característica que irá acontecer para a classe 
+
+# Quando trazemos uma classe, criamos um objeto
+class Cliente(Base):
+    # Tabela 
+    __tablename__ = "clientes"
+
+    #Definindo campos de tabela 
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", String)
+    email = Column("email", String)
+    senha = Column("senha", String)
+
+    def __init__(self, nome: str, email: str, senha: str):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+
+
+Base.metadata.create_allI(bind=MEU_BANCO)
+
+
+#CRUD
+os.system("cls || clear")
+print("Solicitando dados do usuário:")
+inserir_nome = input("Digite seu nome:")
+inserir_email = input("Digite seu email:")
+inserir_senha = input("Digite sua senha:")
+
+cliente = Cliente(nome= inserir_nome, email= inserir_email, senha= inserir_senha)
+session.add(cliente)
+session.commit()
+
+#Read - Select  - Consulta
